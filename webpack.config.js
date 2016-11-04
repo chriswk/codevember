e7837bf2f81f5c1ae891a5fd643ab1f4fa2db662
chrkolst <christopher.kolstad@finn.no>
2016-11-04 09:50:44 +0100

    Set up publish

---
+++ webpack.config.js
@@ -0,0 +1,96 @@
+var path              = require( 'path' );
+var webpack           = require( 'webpack' );
+var merge             = require( 'webpack-merge' );
+var HtmlWebpackPlugin = require( 'html-webpack-plugin' );
+console.log( 'WEBPACK GO!');
+
+// detemine build env
+var TARGET_ENV = process.env.npm_lifecycle_event === 'build' ? 'production' : 'development';
+
+// common webpack config
+var commonConfig = {
+
+  output: {
+    path:       path.resolve( __dirname, 'dist/' ),
+    filename: '[hash].js',
+  },
+
+  resolve: {
+    modulesDirectories: ['node_modules'],
+    extensions:         ['', '.js', '.elm']
+  },
+
+  module: {
+    noParse: /\.elm$/
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject:   'body',
+      filename: 'index.html'
+    })
+  ],
+
+}
+
+// additional webpack settings for local env (when invoked by 'npm start')
+if ( TARGET_ENV === 'development' ) {
+  console.log( 'Serving locally...');
+
+  module.exports = merge( commonConfig, {
+
+    entry: [
+      'webpack-dev-server/client?http://localhost:8080',
+      path.join( __dirname, 'src/index.js' )
+    ],
+
+    devServer: {
+      inline:   true,
+      progress: true
+    },
+
+    module: {
+      loaders: [
+        {
+          test:    /\.elm$/,
+          exclude: [/elm-stuff/, /node_modules/],
+          loader:  'elm-hot!elm-webpack'
+        }
+      ]
+    }
+
+  });
+}
+
+// additional webpack settings for prod env (when invoked via 'npm run build')
+if ( TARGET_ENV === 'production' ) {
+  console.log( 'Building for prod...');
+
+  module.exports = merge( commonConfig, {
+
+    entry: path.join( __dirname, 'src/index.js' ),
+
+    module: {
+      loaders: [
+        {
+          test:    /\.elm$/,
+          exclude: [/elm-stuff/, /node_modules/],
+          loader:  'elm-webpack'
+        }
+      ]
+    },
+
+    plugins: [
+      new webpack.optimize.OccurenceOrderPlugin(),
+
+      // minify & mangle JS/CSS
+      new webpack.optimize.UglifyJsPlugin({
+          minimize:   true,
+          compressor: { warnings: false }
+          // mangle:  true
+      })
+    ]
+
+  });
+}
